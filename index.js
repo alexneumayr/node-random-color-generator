66c89c29c75b84e0c66dc800bfb26b4d5c5327ea
alexneumayr <alex.nmr@outlook.com>
2025-01-14 16:12:10 +0100

    Add functionality to specify hue

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 import chalk from 'chalk';
 import { randomColor } from 'randomcolor';
 
-const color = randomColor();
-
 if (process.argv.length < 3) {
+  const color = randomColor();
+  const chalkColor = chalk.hex(color);
+  console.log(
+    chalkColor(`###############################
+  ###############################
+  ###############################
+  #####                     #####
+  #####       ${color}       #####
+  #####                     #####
+  ###############################
+  ###############################
+  ###############################`),
+  );
+} else if (process.argv.length < 4) {
+  const color = randomColor({
+    hue: process.argv[2],
+    luminosity: 'random',
+  });
   const chalkColor = chalk.hex(color);
   console.log(
     chalkColor(`###############################
