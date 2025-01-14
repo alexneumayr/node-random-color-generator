c0beb347e9ab7f17256c251978233f1cfd038a0c
alexneumayr <alex.nmr@outlook.com>
2025-01-14 20:41:42 +0100

    Refactor the code and fix README

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Node.js Random Color Generator
 
+> [!NOTE]
+> Please note that in the actual application the output will have the same color as the Hex Color Code shown in the middle. Due to limitations of Markdown it was not possible to show that in the examples.
+
 Display a random color by running the application without any arguments:
 
-<span style="color: green">
 ```
 node index.js
 ###############################
@@ -19,82 +21,61 @@ node index.js
 ###############################
 ###############################
 ###############################
-
-#####
-
-##### #ff72d2
-
-#####
-
+#####                     #####
+#####       #ff72d2       #####
+#####                     #####
 ###############################
 ###############################
 ###############################
-
 ```
 
 Display a specific color by providing the color name as an argument:
 
 ```
-
 node index.js red
 ###############################
 ###############################
 ###############################
-
-#####
-
-##### #b72d42
-
-#####
-
+#####                     #####
+#####       #b72d42       #####
+#####                     #####
 ###############################
 ###############################
 ###############################
-
 ```
 
 Display a specific color with a specific luminosity by providing color and luminosity as an argument:
 
 ```
-
 node index.js blue dark
 ###############################
 ###############################
 ###############################
-
-#####
-
-##### #023e8c
-
-#####
-
+#####                     #####
+#####       #023e8c       #####
+#####                     #####
 ###############################
 ###############################
 ###############################
-
 ```
-
-<span style="color: green"> Some green text </span>
 
 When "ask" is used as an argument instead of a color the user will be asked to input a color and a luminosity which will then be displayed afterwards:
 
 ```
-
 node index.js ask
-Enter a color name: blue
-Type in luminosity (dark/light/bright): light
-###############################
-###############################
-############################### ##### #####
-
-##### #c7b4f7
-
-#####
-
+Enter a color name:
+blue
+Type in luminosity (dark/light/bright):
+light
 ###############################
 ###############################
 ###############################
-
+#####                     #####
+#####        #c7b4f7      #####
+#####                     #####
+###############################
+###############################
+###############################
 ```
 
 without any user input: print random color
@@ -103,9 +84,4 @@ with user input of a color name: print the specified color
 
 with user input of a color name and a luminosity: print the specified color in the specified luminosity
 
-## Todo
-
-- [x] Find library which generates random colors
-- [x] Find out how to style console outputs
-- [ ] Find out how to accept user input from command line
-```
+when the argument "ask" is used: asks the user to input and luminosity and then prints it afterwards
--- index.js
+++ index.js
@@ -1,29 +1,40 @@
+import readline from 'node:readline';
 import chalk from 'chalk';
-import { randomColor } from 'randomcolor';
-import readline from 'readline';
+import randomColor from 'randomcolor';
 
+// Function to log the color to the console
+function logColor(hexColor) {
+  console.log(
+    chalk.hex(hexColor)(
+      `###############################
+###############################
+###############################
+#####                     #####
+#####       ${hexColor}       #####
+#####                     #####
+###############################
+###############################
+###############################`,
+    ),
+  );
+}
+
+// If no argument is provided then it will display a random color
 if (process.argv.length < 3) {
   const color = randomColor();
-  const chalkColor = chalk.hex(color);
-  console.log(
-    chalkColor(`###############################
-###############################
-###############################
-#####                     #####
-#####       ${color}       #####
-#####                     #####
-###############################
-###############################
-###############################`),
-  );
+  logColor(color);
 } else if (process.argv.length < 4) {
+  // Checks if one argument is provided
   if (process.argv[2] === 'ask') {
+    // Checks if this argument is "ask"
+    // Creates readline interface the enable command line input functionality
     const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
     });
-
+    // Asks for color name
     rl.question('Enter a color name: ', (userColor) => {
+      // Asks for luminosity
       rl.question(
         'Type in luminosity (dark/light/bright): ',
         (userLuminosity) => {
@@ -31,56 +42,27 @@ if (process.argv.length < 3) {
             hue: userColor,
             luminosity: userLuminosity,
           });
-          console.log(
-            chalk.hex(color)(`###############################
-        ###############################
-        ###############################
-        #####                     #####
-        #####       ${color}       #####
-        #####                     #####
-        ###############################
-        ###############################
-        ###############################`),
-          );
+          logColor(color);
           rl.close();
         },
       );
     });
   } else {
+    // If one argument is provided and this argument is NOT "ask" it will treat it as a color name
     const color = randomColor({
       hue: process.argv[2],
       luminosity: 'random',
     });
-    const chalkColor = chalk.hex(color);
-    console.log(
-      chalkColor(`###############################
-###############################
-###############################
-#####                     #####
-#####       ${color}       #####
-#####                     #####
-###############################
-###############################
-###############################`),
-    );
+    logColor(color);
   }
 } else if (process.argv.length < 5) {
+  // If two arguments are provided it will use the first one as a color and the second as the luminosity
   const color = randomColor({
     hue: process.argv[2],
     luminosity: process.argv[3],
   });
-  const chalkColor = chalk.hex(color);
-  console.log(
-    chalkColor(`###############################
-###############################
-###############################
-#####                     #####
-#####       ${color}       #####
-#####                     #####
-###############################
-###############################
-###############################`),
-  );
+  logColor(color);
 } else {
+  // If more than three arguments are provided it tells the user to refer to the README
   console.log('Please refer to the README to see how to use the application.');
 }
